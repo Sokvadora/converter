d9b16aa5c6b2b6bba35991795b2d29e2add88ae1
Elena <mika.love.kattun@gmail.com>
2019-08-03 02:13:34 +0300

    add currency in real time

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
 // Для курсов валют
 rates = {};
+usd = {};
+eur = {};
 
 $(document).ready(function () {
+
+    let inputRub = $("#rub"),
+        inputUsd = $("#usd"),
+        inputEur = $("#eur");
+
     // Загружаем курсы валют
     $.ajax({
         url: 'https://www.cbr-xml-daily.ru/daily_json.js',
@@ -9,57 +16,70 @@ $(document).ready(function () {
         dataType: 'json',
         success: function (data) {
             rates = data.Valute;
+            usd = rates.USD.Value;
+            eur = rates.EUR.Value;
             console.log(rates);
-            console.log(rates.USD.Value);
-            console.log(Object.keys(rates));
-            console.log(Object.getOwnPropertyNames(rates.USD));
+            console.log(usd);
+            console.log(eur);
         },
         error: function () {
             alert('Не удалось загрузить данные.');
         }
     });
 
-    // При клике на кнопку "рассчитать"
-    $('#calculate').click(function () {
-        let inputRub = $("#rub"),
-            inputUsd = $("#usd"),
-            inputEur = $("#eur");
 
-        // Вычисляем результат
-        rubUsd = inputRub.val() / rates.USD.Value;
-        inputUsd.val(rubUsd.toFixed(2));
+    //вводим рубль
+    $('#rub').bind({
+        keyup: function () {
 
-        rubEur = inputRub.val() / rates.EUR.Value;
-        inputEur.val(rubEur.toFixed(2));
+            rubUsd = inputRub.val() / usd;
+            inputUsd.val(rubUsd.toFixed(4));
 
+            rubEur = inputRub.val() / eur;
+            inputEur.val(rubEur.toFixed(4));
+        },
+        click: function () {
+            $('#rub').val('');
+            $('#usd').val('');
+            $('#eur').val('');
+        }
     });
 
+
+    //вводим доллар
+    $('#usd').bind({
+        keyup: function () {
+
+            usdRub = inputUsd.val() * usd;
+            inputRub.val(usdRub.toFixed(4));
+
+            usdEur = usd / eur * inputUsd.val();
+            inputEur.val(usdEur.toFixed(4));
+        },
+        click: function () {
+            $('#rub').val('');
+            $('#usd').val('');
+            $('#eur').val('');
+        }
+    });
+
+
+    //вводим евро
+    $('#eur').bind({
+        keyup: function () {
+
+            eurRub = inputEur.val() * eur;
+            inputRub.val(eurRub.toFixed(4));
+
+            eurUsd = eur / usd * inputEur.val();
+            inputUsd.val(eurUsd.toFixed(4));
+        },
+        click: function () {
+            $('#rub').val('');
+            $('#usd').val('');
+            $('#eur').val('');
+        }
+    });
+
+
 });
-
-
-
-
-/*let inputRub = document.getElementById('rub'),
-   inputUsd = document.getElementById('usd'),
-   inputEur = document.getElementById('eur');
-
-   //rub 
-    inputRub.addEventListener('input', ()=>{
-       let request = new XMLHttpRequest();
-       //request.open(method,url,async,login,pass);
-       request.open('GET','js/value.json');
-       request.setRequestHeader('Content-type','application/json; charset=utf-8');
-       request.send();
-
-       request.addEventListener('readystatechange', function(){
-           if (request.readyState === 4 && request.status == 200){
-               let data = JSON.parse(request.response);
-
-               inputUsd.value = (inputRub.value/data.usd).toFixed(4);
-               //inputRub.value = (inputRub.value*data.usd).toFixed(4); 
-           } else {
-               inputUsd.value = "Что-то пошло не так!";
-           }
-       });
-   });
-*/
